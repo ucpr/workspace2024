ba9c57ee69174c8dff4f1c05e63cbd044e6b4bdd
ucpr <contact@ucpr.dev>
2024-05-01 13:49:15 +0900

    feat: add test pino gcp logger

---
+++ pino-gcp-logger/index.ts
@@ -0,0 +1,84 @@
+import pino, { type Level, type LoggerOptions } from 'pino';
+
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+const gcpLogSeverity: Record<string, string> = {
+  trace: 'DEBUG',
+  debug: 'DEBUG',
+  info: 'INFO',
+  warn: 'WARNING',
+  error: 'ERROR',
+  fatal: 'CRITICAL',
+}
+
+export interface ServiceContext {
+  serviceName?: string;
+  version?: string;
+  mixin?: (mergeObject: object, level: number) => object;
+}
+
+function gcpLogger(
+  options?: LoggerOptions,
+  context: ServiceContext = {},
+): LoggerOptions {
+  const { serviceName, version, mixin } = context;
+
+  return {
+    level: "info",
+    mixin: (mergeObject: object, _: number) => {
+      return {
+        jsonPayload: {
+          ...mergeObject,
+        },
+        serviceContext: {
+          service: serviceName,
+          version: version,
+        },
+      };
+    },
+    formatters: {
+      level: (label: string) => {
+        const pinoLevel = label as Level;
+        const severity = gcpLogSeverity[pinoLevel] ?? "INFO";
+        return { severity };
+      },
+    },
+    serializers: {
+      err: pino.stdSerializers.err,
+      error: pino.stdSerializers.err,
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+    messageKey: 'message',
+    ...options,
+  }
+}
+
+export class Logger {
+  logger: pino.Logger;
+
+  constructor(serviceName: string, version: string) {
+    this.logger = pino(gcpLogger({}, {
+      serviceName: serviceName,
+      version: version,
+    }));
+  }
+
+  debug(message: String, obj?: Object) {
+    this.logger.debug({ message: message, ...obj });
+  }
+
+  info(message: String, obj?: Object) {
+    this.logger.info({ message: message, ...obj });
+  }
+
+  warn(message: String, obj?: Object) {
+    this.logger.warn({ message: message, ...obj });
+  }
+
+  error(message: String, obj?: Object) {
+    this.logger.error({ message: message, ...obj });
+  }
+
+  critical(message: String, obj?: Object) {
+    this.logger.fatal({ message: message, ...obj });
+  }
+}
